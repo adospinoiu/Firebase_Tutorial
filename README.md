adb6433445466000670b617acc162f7359b812a2
adospinoiu <alex.dospinoiu@gmail.com>
2022-04-26 06:09:41 -0700

    Created entry fields

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 #4 reate the REACT APP with the installation line "npx create-react-app firebase-tutorial-front-end". The last part of the installation line is actually the name of the folder that will be created where the React Install files will be.
 
 #5 Delete the un-necessary files installed by the React Installation: App.test.js; logo.svg and setupTests.js. Remove the 'import logo' from App.js
+
--- firebase-tutorial-front-end/src/App.js
+++ firebase-tutorial-front-end/src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [ data, setData ] = useState({});
+
+  const handleInput = (event) => {
+    let newInput = { [event.target.name]: event.target.value };
+
+    setData( { ...data, ...newInput });
+    
+  }
+
+  const handleSubmit = () => {
+    console.log(data);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={(event) => handleInput(event)}
+        />
+        
+        <input
+          name="password"
+          placeholder="Password"
+          onChange={(event) => handleInput(event)}
+        />
+        
+        <button 
+          onClick={handleSubmit} type="submit">Submit
+        </button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
